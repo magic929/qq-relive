e25ed5f6f156cae6ed84a2fa1db9aeca8bc5123f
magic929 <nyanmina929@gmail.com>
2021-06-11 18:26:37 +0800

    plugins update

--- guild_war/guild_war/plugins/bossinfo/__init__.py
+++ guild_war/guild_war/plugins/bossinfo/__init__.py
@@ -7,11 +7,13 @@ from nonebot.typing import T_State
 import httpx
 
 from .config import plugin_config
-from .data_source import set_boss
+from .data_source import *
 
 boss = CommandGroup('boss', block=True)
 boss_search = boss.command('search', aliases={"信息"}, rule=to_me())
 boss_set = boss.command('set', aliases={"更新"}, rule=to_me())
+boss_team = boss.command('set_team', aliases={"添加队伍"}, rule=to_me())
+boss_notice = boss.command('set_notice', aliases={"添加注意"}, rule=to_me())
 
 serach_api = f'{plugin_config.base_url}{plugin_config.boss_info_path}{plugin_config.search_api}'
 set_api = f'{plugin_config.base_url}{plugin_config.boss_info_path}{plugin_config.set_api}'
@@ -34,39 +36,37 @@ async def serach_boss(key1, key2):
                 notice = res["details"]["notice"]
                 if "teams" in res["details"]:
                     for team in res["details"]["teams"]:
-                        teams += f'{team["team"]}, {team["turn"]}T'
+                        teams += f'{team["team"]}{team["turn"]}T\n                 '
             if "status" in res:
-                hp = res["details"]["hp"]
+                hp = res["status"]["hp"]
 
-            return f'{name}\n难度：{level}\n注意事项：{notice}\n推荐队伍：{teams}\n当前血量：{hp}'
+            return f'{name}\n难度：{level}\n注意事项：{notice}\n推荐队伍：{teams.strip()}\n当前血量：{hp}'
 
-    except (httpx.HTTPError, KeyError):
-        return None
+    except Exception as e:
+        print("error: ", e)
+        return False
     
 
 @boss_search.handle()
 async def get_first_handle_boss_info(bot: Bot, event: MessageEvent, state: T_State):
     args = str(event.get_message()).strip().split(" ")
     if len(args) == 1:
-        if args[0].isdigit():
-            state["level"] = args
-        else:
-            state["name"] = args
+        await boss_search.reject("请重新输入boss名字和难度")
 
     if len(args) == 2:
         state["name"] = args[0]
         state['level'] = args[1]
     
-    return False
-
-
-@boss_search.got("level", prompt="请输入boss难度")
-@boss_search.got("name", prompt="请输入boss名字")
-async def get_handle_boss_info(bot:Bot, event: MessageEvent, state:T_State):
     result = await serach_boss(state["name"], state["level"])
     if result:
         await boss_search.finish(result)
-    await boss_search.reject("查找失败，请输入正确参数")
+    await boss_search.reject("查找失败")
+
+
+
+# @boss_search.got()
+# async def get_handle_boss_info(bot:Bot, event: MessageEvent, state:T_State):
+    
 
 
 @boss_set.handle()
@@ -87,3 +87,45 @@ async def boss_set_do(bot: Bot, event: MessageEvent, state: T_State):
     if result:
         await boss_set.finish("boss情报已记录")
     await boss_set.reject("boss情报记录失败")
+
+
+@boss_team.args_parser
+async def boss_team_parser(bot: Bot, event: Event, state: T_State):
+    args = str(event.get_message()).strip().split(" ")
+    if len(args) < 2:
+        await boss_team.reject("请输入队伍和回数")
+    state["team"] = args[0]
+    state["turn"] = args[1]
+
+
+# @boss_team.handle()
+# async def boss_set_team_handle(bot: Bot, event: MessageEvent, state: T_State):
+#     args = str(event.get_message()).strip().split(" ")
+#     if len(args) < 2:
+#         await boss_team.reject("请输入boss名字和难度")
+#     (state['name'], state['level']) = (args[0], args[1])
+
+
+@boss_team.got('team_turn', prompt="请输入队伍和回数")
+async def boss_set_team(bot: Bot, event: Event, state:T_State):
+    result = await set_team(state["name"], state["level"], state["team"], state["turn"])
+    if result:
+        await boss_team.finish(result)
+    await boss_team.finish("添加失败")
+
+
+@boss_team.handle()
+@boss_notice.handle()
+async def boss_set_notice_handle(bot: Bot, event: MessageEvent, state: T_State):
+    args = str(event.get_message()).strip().split(" ")
+    if len(args) < 2:
+        await boss_notice.reject("请输入boss名字和难度")
+    (state['name'], state['level']) = (args[0], args[1])
+
+
+@boss_notice.got('notice', prompt="请输入注意事项")
+async def boss_set_notice(bot: Bot, event: MessageEvent, state: T_State):
+    result = await set_notice(state["name"], state["level"], state["notice"])
+    if result:
+        await boss_notice.finish(result)
+    await boss_notice.finish("添加失败")
--- guild_war/guild_war/plugins/bossinfo/data_source.py
+++ guild_war/guild_war/plugins/bossinfo/data_source.py
@@ -3,6 +3,8 @@ import httpx
 from .config import plugin_config
 
 set_api = f'{plugin_config.base_url}{plugin_config.boss_info_path}{plugin_config.set_api}'
+set_team_api = f'{plugin_config.base_url}{plugin_config.boss_info_path}{plugin_config.set_team_api}'
+set_notice_api = f'{plugin_config.base_url}{plugin_config.boss_info_path}{plugin_config.set_notice_api}'
 
 async def set_boss(key1, key2):
     print(key1, key2, set_api)
@@ -17,4 +19,36 @@ async def set_boss(key1, key2):
         return True
     
     except (httpx.HTTPError, KeyError):
+        return False
+
+
+async def set_team(key1, key2, key3, key4):
+    paydata = {
+        "name": key1,
+        "level": key2,
+        "team": key3,
+        "turn": key4
+    }
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.post(set_team_api, json=paydata)
+            res = res.json()
+            return res["msg"]
+    except Exception as e:
+        print("error: ", e)
+        return False
+
+async def set_notice(key1, key2, key3):
+    paydata = {
+        "name": key1,
+        "level": key2,
+        "notice": key3
+    }
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.post(set_notice_api, json=paydata)
+            res = res.json()
+            return res["msg"]
+    except Exception as e:
+        print("error", e)
         return False
